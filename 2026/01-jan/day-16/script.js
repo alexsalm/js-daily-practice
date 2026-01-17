03fbee0e131484e2f0206bc43f9cf82b2ce401ad
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-01-16 23:37:46 -0800

    Add h2 tags in index.html file and pass isPrime function result into h2 reference

--- 2026/01-jan/day-16/script.js
+++ 2026/01-jan/day-16/script.js
@@ -1,7 +1,19 @@
 const heading = document.querySelector("h1");
-
+const secondHeading = document.querySelector("h2");
 heading.textContent = future();
+secondHeading.textContent = `23 is a prime number ? ${isPrime(23)}`;
+secondHeading.textContent = `16 is a prime number? ${isPrime(16)}`;
 
 function future() {
     return "You'll never have flying cars";
 }
+
+function isPrime(x) {
+    for (let i = 2; i < x; i++) {
+        if (x % i === 0) return false;
+    }
+
+    return x > 1;
+}
+
+
