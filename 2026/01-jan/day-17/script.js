1931f7b2b34fcccc0379980d29d73ed19721b6c0
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-01-17 23:42:32 -0800

    Create global variables and addToScore function that adds to and prints running score whenever it is called

--- 2026/01-jan/day-17/script.js
+++ 2026/01-jan/day-17/script.js
@@ -1,8 +1,38 @@
 const heading = document.querySelector("h1");
 
+let humanScore = 0;
+let computerScore = 0;
+
+function addToScore(a, b) {
+    if (a > b) {
+        humanScore++;
+    } else if (a < b) {
+        computerScore++;
+    }
+    console.log(`Human score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`);
+
+    if (humanScore == 5) {
+        console.log("The winner is human!");
+        return;
+    } else if (computerScore == 5) {
+        console.log("The winner is computer!");
+        return;
+    }
+}
+
 const horn = () => {
     return "Toot";
 };
 
 heading.textContent = horn();
 
+addToScore(5, 1);
+addToScore(1, 1);
+addToScore(0, 4);
+addToScore(1, 2);
+addToScore(7, 6);
+addToScore(4, 4);
+addToScore(9, 3);
+addToScore(3, 1);
+addToScore(5, 2);
