755a2c6100813eea8dbd4c2ce6e7f30b991a3c8c
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-01-22 11:37:33 -0800

    Add code from previous day

---
+++ 2026/01-jan/day-22/script.js
@@ -0,0 +1,47 @@
+const heading = document.querySelector("h1");
+const button = document.querySelector("button");
+const score = document.querySelector("div");
+const span1 = document.createElement("span");
+
+let humanScore = 0;
+let computerScore = 0;
+
+function addToScore(a, b) {
+    const runningScore = function(currentHumanScore, currentComputerScore) {
+        span1.textContent = (`Human score: ${currentHumanScore} Computer score: ${currentComputerScore}`);
+        score.appendChild(span1);
+    };
+    
+    const displayWinner = function(humanScore, computerScore) {
+        if (humanScore > computerScore) {
+            heading.textContent = "The winner is human!";
+        }
+        else if (humanScore < computerScore) {
+            heading.textContent = "The winner is computer";
+        } 
+        else {
+            heading.textContent = "It's a tie!";
+        }
+    };
+    
+    if (a > b) {
+        humanScore++;
+    } else if (a < b) {
+        computerScore++;
+    }
+
+    runningScore(humanScore, computerScore);
+    
+    if (humanScore == 5) {
+        displayWinner(humanScore, computerScore);
+        return;
+    } else if (computerScore == 5) {
+        displayWinner(humanScore, computerScore);
+        return;
+    }
+}
+
+button.addEventListener('click', function(event) {
+    event.preventDefault();
+    addToScore(3, 1);
+})
