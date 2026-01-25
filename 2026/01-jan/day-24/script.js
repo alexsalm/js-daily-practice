c181120fee5d524b06187c94c97a0bb468729061
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-01-25 10:56:18 -0800

    Add class to all divs created in nested for loop and attempt to style class in css file

--- 2026/01-jan/day-24/script.js
+++ 2026/01-jan/day-24/script.js
@@ -7,6 +7,7 @@ for (let y = 0; y < size; y++) {
     for (let x = 0; x < size; x++) {
         grid += "X";
         const div = document.createElement("div");
+        div.classList.add("grid-square");
         container.appendChild(div);
     }
     grid += "\n";
