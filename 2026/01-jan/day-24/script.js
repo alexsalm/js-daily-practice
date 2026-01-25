5b522ec561523f99c33158e6039881d22f6f5b5a
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-01-25 00:14:51 -0800

    Create references to container and div, attempt to append div to container using existing nested for loop

--- 2026/01-jan/day-24/script.js
+++ 2026/01-jan/day-24/script.js
@@ -1,9 +1,13 @@
+const div = document.createElement("div");
+const container = document.querySelector(".container");
+
 let size = 16;
 let grid = "";
 
 for (let y = 0; y < size; y++) {
     for (let x = 0; x < size; x++) {
         grid += "X";
+        container.appendChild(div);
     }
     grid += "\n";
 }
