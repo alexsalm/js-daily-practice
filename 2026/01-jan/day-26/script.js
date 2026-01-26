bdf63fd8103b1e32abd09d89042eb585ae03150d
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-01-26 10:33:31 -0800

    Copy js code from previous day

---
+++ 2026/01-jan/day-26/script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+
+let size = 16;
+let grid = "";
+
+for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+        grid += "X";
+        const div = document.createElement("div");
+        div.classList.add("grid-square");
+        div.textContent = "Hello World!";
+        container.appendChild(div);
+    }
+    grid += "\n";
+}
+
+console.log(grid);
