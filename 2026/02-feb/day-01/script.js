cdddf8f7b91a3021fc0d8eb2d48e7d18a1407a6c
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-01 11:16:00 -0800

    Practice concatenating lengthy template literals within console.logs

--- 2026/02-feb/day-01/script.js
+++ 2026/02-feb/day-01/script.js
@@ -1,8 +1,19 @@
-const city = {
+const city1 = {
     name: "Amsterdam",
     population: 1.2,
     country: "The Netherlands",
     introduceCity: function() {
-        console.log(`My favorite city so far is ${this.name}. It has a population of roughly ${this.population} million and is located in ${this.country}.`);
+        console.log(`My favorite city so far is ${this.name}. It has a population` + 
+        ` of roughly ${this.population} million and is located in ${this.country}.`);
     }
+}
+
+const city2 = {
+    name: "Tokyo",
+    population: 14,
+    country: "Japan",
+    introduceCity() {
+        console.log(`My second favorite city is ${this.name}. It has a population` +
+        ` of about ${this.population} million in the city proper.`);
+    }   
 }
