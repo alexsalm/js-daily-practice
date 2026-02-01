64dd27b19599d46f6e1553e3227d0fb52749984e
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-01 00:01:22 -0800

    Create country object and assign it some properties and function(s)

--- 2026/01-jan/day-31/script.js
+++ 2026/01-jan/day-31/script.js
@@ -8,3 +8,16 @@ const person = {
         console.log(`Hi! I'm ${this.name[0]}.`);
     },
 };
+
+const country = {
+    name: "Netherlands",
+    capital: "Amsterdam", 
+    continent: "Europe",
+    language: "Dutch",
+    population: 18_000_000,
+    introduceCountry: function () {
+        console.log(`The ${this.name} is a country of roughly ${this.population} people. It is located in ${this.continent}. Its capital city is ${this.capital}`);
+    }
+};
+
+
