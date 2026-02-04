eefad00bc01b04a97d323d14deb026a669caad1b
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-04 10:56:02 -0800

    Add comments that reinforce understanding of new operator and references

--- 2026/02-feb/day-04/script.js
+++ 2026/02-feb/day-04/script.js
@@ -5,6 +5,10 @@ function Person(name) {
     };
 }
 
+// the new operator creates empty object and executes constructor, 
+// returns newly created object
+// alex binding holds reference to object instance itself
+// references are pointers to values stored in variables
 const alex = new Person("Alex");
 alex.introduceSelf();
 
