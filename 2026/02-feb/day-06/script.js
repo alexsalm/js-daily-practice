639724535b0dd966fc071805af585ace04cb37bb
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-06 11:49:52 -0800

    Create robot objects and put them in an array

--- 2026/02-feb/day-06/script.js
+++ 2026/02-feb/day-06/script.js
@@ -11,3 +11,23 @@ let ages = users.map(item => item.age);
 
 let lengths = ["Bilbo", "Gandalf", "Nazgul"].map(item => item.length);
 alert(lengths);
+
+let paul = {name: "Paul", surname: "Smith", id: 1};
+let stan = {name: "Stan", surname: "Hunt", id: 2};
+let rex = {name: "Rex", surname: "Key", id: 3};
+
+let users2 = [paul, stan, rex];
+
+let usersMapped = users2.map(item => ({
+    fullName: `${item.name} ${item.surname}`,
+    id: item.id
+}));
+
+alert(usersMapped[0].id);
+alert(usersMapped[0].fullName);
+
+let korg = {name: "Korg", age: 25};
+let jik = {name: "Jik", age: 30};
+let brin = {name: "Brin", age: 28};
+
+let arr = [korg, jik, brin];
