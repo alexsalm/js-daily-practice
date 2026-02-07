0eec1eaa2039e7c23c4d274d3117e493e7092eeb
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-07 12:16:43 -0800

    Write function that gets the average age of users in an array of objects

--- 2026/02-feb/day-07/script.js
+++ 2026/02-feb/day-07/script.js
@@ -13,3 +13,18 @@ sortByAge(arr);
 alert(arr[0].name);
 alert(arr[1].name);
 alert(arr[2].name);
+
+function getAverageAge(arr) {
+    let addedAges = 0;
+    let averageAge = 0;
+    for (let i = 0; i < arr.length; i++){
+        addedAges += arr[i].age;
+    }
+    
+    averageAge = Math.round(addedAges / arr.length);
+    return averageAge;
+}
+
+alert(getAverageAge(arr));
+
+console.log(arr[0]);
