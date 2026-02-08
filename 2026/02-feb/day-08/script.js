43c0493ea16c7eb3cd10736e1844e988ea36bd96
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-08 11:52:06 -0800

    Get more familiar with setting keys using bracket notation on objects

--- 2026/02-feb/day-08/script.js
+++ 2026/02-feb/day-08/script.js
@@ -11,3 +11,18 @@ let users = [
 ];
 
 console.log(users);
+
+// The result of the previous call becomes the first argument 
+// of the next one
+// On the first run, obj is an empty object literal
+// an object literal is basically "let object = {};" w/ or w/o members
+// "obj" is the initial value
+// "value" is the first array element
+function groupById(array) {
+    return array.reduce((obj, value) => {
+        obj[value.id] = value;
+        return obj;
+    }, {})
+}
+// the object literal starts off empty
+console.log(groupById(users));
