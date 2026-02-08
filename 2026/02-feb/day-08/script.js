7adcfff67d5d8cebd81cbaf7525b3ee2a78ae743
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-08 10:47:27 -0800

    Create array of 'user' objects

--- 2026/02-feb/day-08/script.js
+++ 2026/02-feb/day-08/script.js
@@ -3,3 +3,11 @@ let arr = [1, 2, 3, 4, 5];
 let result = arr.reduce((sum, current) => sum + current, 0);
 
 console.log(`The result of adding all elements in arr is ${result}`);
+
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24}, 
+    {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+console.log(users);
