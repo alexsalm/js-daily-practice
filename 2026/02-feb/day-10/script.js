1ef0545e220539826714a2ea7922e243368b5daf
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-10 12:09:04 -0800

    Create person object and assign properties to it, test that object holds said properties using console.log

---
+++ 2026/02-feb/day-10/script.js
@@ -0,0 +1,10 @@
+let person = {
+    name: "Alex",
+    age: 32,
+    occupation: "Developer",
+    favoriteCity: "Amsterdam",
+    hobbies: ["Work out", "cook", "read", "code", "go for walks"],
+};
+
+console.log(`${person.name} is ${person.age} years old`);
+console.table(person.hobbies);
