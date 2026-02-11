1191e5fab574940439651d34593de618c8f2840f
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-11 11:57:16 -0800

    Add statement that prints odd if number is odd

--- 2026/02-feb/day-11/script.js
+++ 2026/02-feb/day-11/script.js
@@ -6,7 +6,10 @@ function min(a, b) {
 console.log(min(17, 9));
 
 function isEven(a) {
-  if (a % 2 == 0) console.log(`${a} is even`);  
+  if (a % 2 == 0) console.log(`${a} is even`); 
+  if (a % 2 == 1) console.log(`${a} is odd`); 
 };
 
 isEven(20);
+isEven(21);
+isEven(69);
