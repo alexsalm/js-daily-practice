4f8b8a5fdd6769fb038d3a8455d6f9354544332d
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-13 23:31:47 -0800

    Write isEven function that returns true if argument is even number, false otherwise

--- 2026/02-feb/day-13/script.js
+++ 2026/02-feb/day-13/script.js
@@ -13,3 +13,17 @@ function findSolution(target) {
 }
 
 console.log(findSolution(24));
+
+let number = 2;
+if (number % 2 == 0) console.log("This number is even");
+
+function isEven(wholeNumber) {
+    if (wholeNumber == 0) return true;
+    else if (wholeNumber == 1) return false;
+    else if (wholeNumber < 0) return isEven(-wholeNumber);
+    else return isEven(wholeNumber - 2);
+}
+
+console.log(isEven(75));
+console.log(isEven(50));
+console.log(isEven(-1));
