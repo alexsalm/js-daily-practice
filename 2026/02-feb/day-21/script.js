8be61a5b880443945d01b6316e0ca62d61b67a91
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-21 10:09:35 -0800

    Practice writing range and sum functions that create an array and print summed array, respectively

---
+++ 2026/02-feb/day-21/script.js
@@ -0,0 +1,22 @@
+function range(start, end, step = start < end ? 1 : -1) {
+    let array = [];
+    if (step > 0) {
+        for (let i = start; i <= end; i += step) {
+            array.push(i);
+        }
+    } else {
+        for (let i = start; i >= end; i += step) {
+            array.push(i);
+        }
+    }
+    return array;
+}
+
+console.table(range(1, 10));
+
+function sum(arr) {
+    let summedArray = arr.reduce((sum, current) => sum + current, 0);
+    console.log(summedArray);
+}
+
+sum(range(1,10));
