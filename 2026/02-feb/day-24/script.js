712fafb0cedd280abf5bb1758edb5c93f97b69ca
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-24 10:15:15 -0800

    Practice writing range and sum functions from scratch

---
+++ 2026/02-feb/day-24/script.js
@@ -0,0 +1,20 @@
+function range(start, end, step = start < end ? 1 : -1) {
+    let array = [];
+    if(step > 0) {
+        for (let i = start; i <= end; i += step) {
+            array.push(i);
+        }
+    } else {
+        for (let i = start; i >= end; i += step) {
+            array.push(i);
+        }   
+    }
+    return array;
+}
+
+function sum(array) {
+    let result = array.reduce((sum, current) => sum + current, 0);
+    return result;
+}
+
+console.log(sum(range(1, 21)));
