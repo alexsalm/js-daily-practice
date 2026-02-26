40641ae546a3e12d2793af19862bf4fc9f0020ad
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-02-26 09:49:41 -0800

    Rewrite flatten function and use for of loop to attempt to create an array containing non-array elements

---
+++ 2026/02-feb/day-26/script.js
@@ -0,0 +1,13 @@
+function flatten(array) {
+    let flattenedArray = [];
+    for (let item of array) {
+        flattenedArray.push(item);
+    }
+    return flattenedArray;
+}
+
+let arrayOfArrays = [[4, 5, 6], [1, 2, 3], [7, 8, 9]];
+
+console.log(arrayOfArrays);
+console.log(flatten(arrayOfArrays));
+
