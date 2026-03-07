64b5916bf44f49d49627ed6b4a2fd6480b6ecfd8
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-03-06 22:09:00 -0800

    Attempt to distinguish between different prototypes in Rabbit example

--- 2026/03-mar/day-06/script.js
+++ 2026/03-mar/day-06/script.js
@@ -1,11 +1,20 @@
 class Rabbit {
-    constructor(type) {
+    constructor(type, age) {
         this.type = type;
+        this.age = age;
     }
     speak(line) {
         console.log(`The ${this.type} rabbit says "${line}".`);
     }
+
+    printAge() {
+        console.log(`The ${this.type} rabbit is ${this.age} years old`);
+    }
 }
 
-let wiseRabbit = new Rabbit("wise");
+let wiseRabbit = new Rabbit("wise", 99);
 console.log(wiseRabbit.speak("stay optimistic"));
+console.log(wiseRabbit.printAge());
+
+console.log(Object.getPrototypeOf(Rabbit) == Function.prototype);
+console.log(Object.getPrototypeOf(wiseRabbit) == Rabbit.prototype);
