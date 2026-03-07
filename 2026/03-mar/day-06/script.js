33f744ea734c6314f1190ec0fd9cf9d7a626cb37
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-03-06 21:48:11 -0800

    Declare Rabbit class and create instance object

---
+++ 2026/03-mar/day-06/script.js
@@ -0,0 +1,11 @@
+class Rabbit {
+    constructor(type) {
+        this.type = type;
+    }
+    speak(line) {
+        console.log(`The ${this.type} rabbit says "${line}".`);
+    }
+}
+
+let wiseRabbit = new Rabbit("wise");
+console.log(wiseRabbit.speak("stay optimistic"));
