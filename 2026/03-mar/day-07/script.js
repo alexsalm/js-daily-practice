d8a587ec95d4f269d9ee0f90f3d0991c83faa5e2
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-03-07 11:11:45 -0800

    Add another method do Rabbit class

--- 2026/03-mar/day-07/script.js
+++ 2026/03-mar/day-07/script.js
@@ -1,12 +1,17 @@
 class Rabbit {
-    constructor(type, age) {
+    constructor(type, age, countryOfOrigin) {
         this.type = type;
         this.age = age;
+        this.countryOfOrigin = countryOfOrigin;
     }
     speak(line) {
         console.log(`The ${this.type} rabbit says "${line}".`);
     }
+    print(birthPlace) {
+        console.log(`The ${this.type} rabbit was born in ${this.countryOfOrigin}.`);
+    }
 }
 
-let wiseRabbit = new Rabbit("wise", 99);
+let wiseRabbit = new Rabbit("wise", 99, "Japan");
 console.log(wiseRabbit.speak("stay optimistic"));
+console.log(wiseRabbit.print());
