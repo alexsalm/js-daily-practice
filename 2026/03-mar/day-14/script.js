9efeaee2922dc99decd81d5489f0031a8cb267bd
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-03-14 23:56:12 -0700

    Write a nested for loop that returns two numbers whose sum is 0 and false otherwise

--- 2026/03-mar/day-14/script.js
+++ 2026/03-mar/day-14/script.js
@@ -9,3 +9,21 @@ class Rabbit {
 
 let wiseRabbit = new Rabbit("wise");
 console.log(wiseRabbit.speak("You can do anything you set your mind to."));
+
+let array1 = [1, 2, 3];
+
+let array2 = [1, 2, -1];
+
+function sumIsZero(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; i < array.length; j++) {
+            if (array[i] + array[j] == 0) {
+                return `${array[i]} + ${array[j]} is equal to 0`;
+            }
+        }
+        return false;
+    }
+}
+
+console.log(sumIsZero(array2));
+console.log(sumIsZero(array1));
