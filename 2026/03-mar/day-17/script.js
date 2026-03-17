7dc6f84373944d4472c97ebd7aed7e66cc63c3bb
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-03-17 10:36:04 -0700

    Practice overriding properties [again]

--- 2026/03-mar/day-17/script.js
+++ 2026/03-mar/day-17/script.js
@@ -9,3 +9,9 @@ class Rabbit {
 
 let rabbit1 = new Rabbit("wise");
 console.log(rabbit1.speak("it pays to be patient"));
+
+Rabbit.prototype.teeth = "small";
+console.log(`rabbit1 has ${rabbit1.teeth} teeth`);
+
+Rabbit.prototype.teeth = "long, sharp, and bloody";
+console.log(`rabbit1 has ${rabbit1.teeth} teeth`);
