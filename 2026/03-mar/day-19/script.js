49b6005918bfdf0b98b71ce9b4149926fe9b079b
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-03-19 10:35:27 -0700

    Write an object that maps names to ages

---
+++ 2026/03-mar/day-19/script.js
@@ -0,0 +1,9 @@
+let ages = {
+    Boris: 39,
+    Liang: 22,
+    Júlia: 62
+};
+
+console.log(`Júlia is ${ages["Júlia"]}`);
+console.log(`Boris is ${ages["Boris"]}`);
+console.log(`Liang is ${ages["Liang"]}`);
