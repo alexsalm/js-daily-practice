01e11e344cfb7be4493b3f7e4a9bd99913044596
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-03-22 11:45:00 -0700

    Add code from previous day and modify printGICategory method

---
+++ 2026/03-mar/day-22/script.js
@@ -0,0 +1,24 @@
+class Food {
+    constructor(type, glycemicIndex, category) {
+        this.type = type;
+        this.glycemicIndex = glycemicIndex;
+        this.category = category;
+    }
+    printBloodGlucoseLevel() {
+        console.log(`${this.type} have a glycemic index of ${this.glycemicIndex}.`);
+    }
+    printGICategory() {
+        console.log(`${this.type} are in the ${this.category} glycemic category.`);
+        if (this.category == "red") {
+            console.log(`${this.type} have a high glycemic index. Choose these LEAST often.`);
+        } else if (this.category == "yellow") {
+            console.log(`${this.type} have a medium glycemic index. Choose these LESS often.`);
+        } else {
+            console.log(`${this.type} have a low glycemic index. Choose these MOST often.`);
+        }
+    }
+}
+
+let quickOats = new Food("Quick oats", 70, "yellow");
+console.log(quickOats.printBloodGlucoseLevel());
+console.log(quickOats.printGICategory());
