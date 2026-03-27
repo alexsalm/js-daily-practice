098819ac1d3bee1f757764eb61ce5af0b6077d70
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-03-27 11:55:44 -0700

    Add another instance of Food class

--- 2026/03-mar/day-27/script.js
+++ 2026/03-mar/day-27/script.js
@@ -5,16 +5,16 @@ class Food {
         this.category = category;
     }
     printBloodGlucoseLevel() {
-        console.log(`${this.type} have a glycemic index of ${this.glycemicIndex}.`);
+        console.log(`${this.type} has/have a glycemic index of ${this.glycemicIndex}.`);
     }
     printGICategory() {
-        console.log(`${this.type} are in the ${this.category} glycemic category.`);
+        console.log(`${this.type} is/are in the ${this.category} glycemic category.`);
         if (this.category == "red") {
-            console.log(`${this.type} have a high glycemic index. Choose these LEAST often.`);
+            console.log(`${this.type} has/have a high glycemic index. Choose this/these LEAST often.`);
         } else if (this.category = "yellow") {
-            console.log(`${this.type} have a medium glycemic index. Choose these LESS often.`);
+            console.log(`${this.type} has/have a medium glycemic index. Choose this/these LESS often.`);
         } else {
-            console.log(`${this.type} have a low glycemic index. Choose these MOST often.`);
+            console.log(`${this.type} has/have a low glycemic index. Choose this/these MOST often.`);
         }
     }
 }
@@ -22,3 +22,7 @@ class Food {
 let quickOats = new Food("Quick oats", 70, "yellow");
 console.log(quickOats.printBloodGlucoseLevel());
 console.log(quickOats.printGICategory());
+
+let hummus = new Food("Hummus", 55, "green");
+console.log(hummus.printBloodGlucoseLevel());
+console.log(hummus.printGICategory());
