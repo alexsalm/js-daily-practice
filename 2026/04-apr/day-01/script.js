c22c73572d72639249dcbfd560fc2da176fbc546
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-04-01 10:01:55 -0700

    Add problem instructions as comments

--- 2026/04-apr/day-01/script.js
+++ 2026/04-apr/day-01/script.js
@@ -13,4 +13,6 @@ const getTheTitles = function() {
 
 };
 
+// Your job is to write a function that takes the array and returns an array of titles:
+// You should use a built-in javascript method to do most of the work for you!
 getTheTitles(books) // ['Book', 'Book2']
