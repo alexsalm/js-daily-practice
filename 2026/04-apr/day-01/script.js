d831c13a7fbb121cbb7b6458b5fa9b04e0dfd3ef
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-04-01 10:00:31 -0700

    Add array of book objects and declare getTheTitles function

---
+++ 2026/04-apr/day-01/script.js
@@ -0,0 +1,16 @@
+const books = [
+    {
+        title: "Book",
+        author: "Name"
+    },
+    {
+        title: "Book2",
+        author: "Name2"
+    }
+]
+
+const getTheTitles = function() {
+
+};
+
+getTheTitles(books) // ['Book', 'Book2']
