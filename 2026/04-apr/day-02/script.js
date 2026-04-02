9f90db6a775ffa26c065cf7a308addc0aca11259
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-04-02 10:11:29 -0700

    Use map() method to attempt to solve t.o.p. exercise

--- 2026/04-apr/day-02/script.js
+++ 2026/04-apr/day-02/script.js
@@ -9,10 +9,11 @@ const books = [
     }
 ]
 
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const bookTitles = array.map((item) => item);
+    return bookTitles;
 };
 
 // Your job is to write a function that takes the array and returns an array of titles:
 // You should use a built-in javascript method to do most of the work for you!
-getTheTitles(books) // ['Book', 'Book2']
+console.log(getTheTitles(books)); // ['Book', 'Book2']
