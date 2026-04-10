6572e7399c27ced8ac7c025dd05adf515e23668a
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-04-10 10:35:14 -0700

    Review reduce() method

--- 2026/04-apr/day-10/script.js
+++ 2026/04-apr/day-10/script.js
@@ -1,3 +1,17 @@
 const findTheOldest = function() {
 
 };
+
+const array = [1, 2, 3, 4];
+
+const initialValue = 0;
+
+// Syntax --> reduce(callBackFn, initialValue)
+// On first call, accumulator is initialValue, otherwise it's array[0]
+// On first call, currentValue is array[0] if initalValue is specified, otherwise it's array[1]
+// 1) acc = 0, currentValue = 1, 2) acc = 1, currentValue = 2, 3) acc = 3, currentValue = 3, 4) acc = 6, currentValue = 4, 5) acc = 10
+const sumWithInitial = array.reduce((accumulator, currentValue) => accumulator + currentValue, 
+initialValue,
+);
+
+console.log(sumWithInitial);
