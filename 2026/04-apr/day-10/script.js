61236f4946d2e2bc3c29a9d4a723e3e145fc2acd
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-04-10 20:47:26 -0700

    Call findTheOldest() function to see it properly return an object

--- 2026/04-apr/day-10/script.js
+++ 2026/04-apr/day-10/script.js
@@ -22,6 +22,24 @@ const getAge = function(birth, death) {
 
 console.log(`I am ${getAge(1993, 2140)} years old`);
 
+const people = [
+	{
+		name : "Carly",
+		yearOfBirth: 1942,
+		yearOfDeath: 1970,
+	},
+	{
+		name: "Ray",
+		yearOfBirth: 1962,
+		yearOfDeath: 2011,
+	},
+	{
+		name: "Jane", 
+		yearOfBirth: 1912,
+		yearOfDeath: 1941,
+	},
+]
+
 const findTheOldest = function (people) {
 	return people.reduce((oldest, currentPerson) => {
 		const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
@@ -32,3 +50,8 @@ const findTheOldest = function (people) {
 		return oldestAge < currentAge ? currentPerson : oldest;
 	});
 };
+
+console.log(findTheOldest(people));
+console.log(getAge(1962, 2011));
+console.log(getAge(1942, 1970));
+console.log(getAge(1912, 1941));
