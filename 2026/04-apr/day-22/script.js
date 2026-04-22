731d3b695f1a22f42bb000032fe3d63013f013d1
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-04-22 10:21:42 -0700

    Copy files from day-22 folder

---
+++ 2026/04-apr/day-22/script.js
@@ -0,0 +1,25 @@
+const myLEDColors = [
+    "red", 
+    "blue", 
+    "amber", 
+    "green", 
+    "purple"
+];
+
+const generateColor = document.querySelector(".generateColor");
+
+generateColor.addEventListener("click", function() {
+    const randomColor = myLEDColors[(Math.floor(Math.random() * (myLEDColors.length)))];
+    const textOnScreen = document.querySelector("#textOnScreen");
+    textOnScreen.textContent = randomColor;
+    console.log(`Random color: ${randomColor}`);
+
+    let stringifiedColor = JSON.stringify(randomColor);
+    console.log(`The color ${stringifiedColor} is now a JSON encoded string.`);
+    console.log(`The same stringified color has now been parsed: ${JSON.parse(stringifiedColor)}`);
+
+    let colorArray = [];
+    let nameOfValue = "color-value";
+    colorArray.push(stringifiedColor);
+    localStorage.setItem(nameOfValue, colorArray);
+});
