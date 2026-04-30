f41a26b71b020204d7429c75098e545a3efb2bab
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-04-30 11:34:24 -0700

    Implement localStorage functionality within button click event

--- 2026/04-apr/day-30/script.js
+++ 2026/04-apr/day-30/script.js
@@ -4,4 +4,13 @@ addCountryButton.addEventListener("click", function() {
 	let textInput = document.getElementById('userInput').value;
 	const textOnScreen = document.querySelector("#textOnScreen");
 	textOnScreen.textContent = textInput;
+
+	let stringifiedCountry = JSON.stringify(textInput);
+	let countryArray = []; // starts off empty
+	let nameOfCountry = "country-value";
+	countryArray.push(stringifiedCountry);
+	localStorage.setItem(nameOfCountry, countryArray);
 });
+
+
+
