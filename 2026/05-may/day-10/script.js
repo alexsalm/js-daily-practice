aa3e4be7336bfecf2e1c0a2b315eb3da8b195ae3
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-05-10 12:08:42 -0700

    Implement function to save todo item to localStorage, put function calls in single DOMContentLoaded event listener

--- 2026/05-may/day-10/script.js
+++ 2026/05-may/day-10/script.js
@@ -1,21 +1,19 @@
 const addCountryButton = document.querySelector(".add-country");
+const addTodoItem = document.querySelector(".add-todo");
 let countryArray = []; // starts off empty
+let todoArray = [];
 
 document.addEventListener("DOMContentLoaded", function () {
 	addCountryButton.addEventListener("click", function() {	
-		let textInput = document.getElementById('userInput').value;
-		const textOnScreen = document.querySelector("#textOnScreen");
-		textOnScreen.textContent = textInput;
-
-		let stringifiedCountry = JSON.stringify(textInput);
-		let nameOfCountry = "country-value";
-		countryArray.push(stringifiedCountry);
-		localStorage.setItem(nameOfCountry, countryArray);
+		saveToLocalStorage();
 	});	
+	addTodoItem.addEventListener("click", function() {
+	saveTodoToLocalStorage();
+	});
 });
 
 
-function saveToLocalStorage() {
+function saveCountryToLocalStorage() {
 	let textInput = document.getElementById('userInput').value;
 	const textOnScreen = document.querySelector("#textOnScreen");
 	textOnScreen.textContent = textInput;
@@ -24,4 +22,15 @@ function saveToLocalStorage() {
 	let nameOfCountry = "country-value";
 	countryArray.push(stringifiedCountry);
 	localStorage.setItem(nameOfCountry, countryArray);
+}
+
+function saveTodoToLocalStorage() {
+	let todoInput = document.getElementById('todoList').value;
+	const todoOnScreen = document.querySelector('#todoOnScreen');
+	todoOnScreen.textContent = todoInput;
+
+	let stringifiedTodo = JSON.stringify(todoInput);
+	let nameOfTodo = "todo-value";
+	todoArray.push(stringifiedTodo);
+	localStorage.setItem(nameOfTodo, todoArray);
 }
