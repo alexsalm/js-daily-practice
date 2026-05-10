db020bb65f2c38f10da823af6125da0411a63be0
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-05-09 18:19:57 -0700

    Remove event listener that was not properly implemented

--- 2026/05-may/day-09/script.js
+++ 2026/05-may/day-09/script.js
@@ -3,14 +3,6 @@ let countryArray = []; // starts off empty
 
 document.addEventListener("DOMContentLoaded", function () {
 	addCountryButton.addEventListener("click", function() {	
-		textInputEventListener("keydown", (event) => {
-			if (event.key === "Enter") {
-				event.preventDefault();
-				console.log("Enter was pressed");
-				saveToLocalStorage();
-			}
-		})
-		
 		let textInput = document.getElementById('userInput').value;
 		const textOnScreen = document.querySelector("#textOnScreen");
 		textOnScreen.textContent = textInput;
