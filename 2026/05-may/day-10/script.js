efb820b7800ccdb0e0f4654f15ab7ed787eaf88e
alexsalm <154659521+alexsalm@users.noreply.github.com>
2026-05-10 12:09:21 -0700

    Fix indentation in DOMContentLoaded event listener

--- 2026/05-may/day-10/script.js
+++ 2026/05-may/day-10/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		saveToLocalStorage();
 	});	
 	addTodoItem.addEventListener("click", function() {
-	saveTodoToLocalStorage();
+		saveTodoToLocalStorage();
 	});
 });
 
